3d842cb5c0074c67e53ee49bd88f02e77c913b72
Ben Stoller <stollersystemsound@gmail.com>
2020-09-14 14:43:25 -0700

    start building main.js character creation logic to handle input from form

--- src/main.js
+++ src/main.js
@@ -2,3 +2,15 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import Game from './js/game_class.js';
+
+
+$(document).ready(function(){
+  let game = new Game([],[],[],[])
+  game.addEnvironment("Castle Room","dark and scary",[],[],[],[])
+  // conditional for class here?
+  let player1 = game.addPlayer(name,race,pclass,level,xp,hp,mp,hunger,inv,str,dex,con,wis,int,chr,lck
+
+  game.environments[0].players.push(player1)
+
+});
