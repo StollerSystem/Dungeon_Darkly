9a5cb682219cf6cfabe806312861ba7a6f002ed7
Ben Stoller <stollersystemsound@gmail.com>
2020-09-14 14:58:14 -0700

    add conditional to handle player class

--- src/main.js
+++ src/main.js
@@ -8,9 +8,41 @@ import Game from './js/game_class.js';
 $(document).ready(function(){
   let game = new Game([],[],[],[])
   game.addEnvironment("Castle Room","dark and scary",[],[],[],[])
-  // conditional for class here?
-  let player1 = game.addPlayer(name,race,pclass,level,xp,hp,mp,hunger,inv,str,dex,con,wis,int,chr,lck
+  
 
-  game.environments[0].players.push(player1)
+  $("form").submit(function(event){
+    event.preventDefault();
+    const nameInput = $("#name").val();
+    const raceInput = $("#race").val();
+    const pclassInput = $("#pclass").val();
+    const strInput = $("#str").val();
+    const dexInput = $("#dex").val();
+    const conInput = $("#con").val();
+    const wisInput = $("#wis").val();
+    const intInput = $("#int").val();
+    const chrInput = $("#chr").val();
+    const lckInput = $("#lck").val();
+    let hp;
+    let mp;
+    
+    switch (pclassInput) {
+      case ("Mage"):
+        hp = 6;
+        mp = 10;
+        break;
+      case ("Warrior"):
+        hp = 12;
+        mp = 0;
+        break;
+      default:
+        hp = 10;
+        mp = 0;
+        break;
+    }
+    
+    let player1 = game.addPlayer(nameInput,raceInput,pclassInput,1,0,hp,mp,0,[],strInput,dexInput,conInput,wisInput,intInput,chrInput,lckInput);
+    game.environments[0].players.push(player1);    
+  })
+  
 
 });
