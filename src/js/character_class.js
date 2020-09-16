cc8dfae8384cbdb64ae8003092c33343368c67fe
3Emme <emmettbuentiempo@gmail.com>
2020-09-16 00:49:56 -0700

    refactor combat functions, including attackRoll to use bare hands

--- src/js/character_class.js
+++ src/js/character_class.js
@@ -123,14 +123,21 @@ export class Character {
     this.baseAc += totalAcBonus;
   }
 
-  attackRoll(weapon){
-    let attackMod = this.abilityScores.scoreMod[weapon.att[0]]+weapon.att[1]+this.level;
+  attackRoll(){
+    let weapon;
+    let attackMod;
+    if (!this.equip.mainHand[0]){
+      weapon = this.equip.mainHand[0];
+      attackMod = this.abilityScores.scoreMod[weapon.atk[0]]+weapon.atk[1]+this.level;
+    } else {
+      attackMod = this.abilityScores.scoreMod('str')+this.level; 
+    }
     return this.roll(1,20,attackMod);
   }
 
   damageRoll(){
     let weapon = this.equip.mainHand[0];
-    let damageMod = this.abilityScores.scoreMod[weapon.att[0]];
+    let damageMod = this.abilityScores.scoreMod[weapon.atk[0]];
     let damageDiceNumber = weapon.dam[0];
     let damageDiceSides = weapon.dam[2];
     return this.roll(damageDiceNumber,damageDiceSides,damageMod);
