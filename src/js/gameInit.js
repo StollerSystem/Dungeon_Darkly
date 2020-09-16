eca04bdd5559ca8880e3b0fbceb5629ed5a9f5cb
Janet Karpenske <janetkarpenske@gmail.com>
2020-09-16 13:36:14 -0700

    Add styling

--- src/js/gameInit.js
+++ src/js/gameInit.js
@@ -39,8 +39,8 @@ export default class GameInit {
     // these are rooms that may or may not be implemented! Add and delete these at your own leisure!
     game.addEnvironment("Castle Entrance", "  Whew! That was quite the trek! But you've finally found it! Ravenhill Castle... You've heard a lot about it. Some strange things have been going on here, including, but not limited to kidnappings, experiments, torture... horrendous interior decorating... such HORRORS! And they must be stopped! Go now my friend! Enter the castle gates with CMD Move!", [], [], [], []);
     game.addEnvironment("Foyer", "  Quite the entrance! The room is filled with grandiose decor, with a huge piano, some suits of armor, a massive chandelier worth more than its weight in gold, and plenty of other trinkets the owner has most likely acquired COMPLETELY legally in their past adventures. There's two stairways and multiple hallways but feel a strange energy towards the corridor in front of you.", [], [], [], []);
-    game.addEnvironment("Library", "", [], [], [], []);
-    game.addEnvironment("Stairs Landing", "", [], [], [], []);
+    game.addEnvironment("Dining Hall", "  Jeez, the table in here is longer than the distance it took you to get to the castle... Does this guy really have that many friends? Nevertheless the table seemed to be being prepared at some point, but was interrupted. The a fire burns bright in the fireplace and only half of the chairs seem to have any settings. There is some sort of smell in the air though. And there's a door open on the other side of the room...", [], [], [], []);
+    game.addEnvironment("Kitchen", "  ", [], [], [], []);
     game.addEnvironment("Stairs Landing", "", [], [], [], []);
     game.addEnvironment("Stairs Landing", "", [], [], [], []);
     return game;
