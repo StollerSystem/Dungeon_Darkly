d5ed0a468f17dbfa3072fb75304850f0cfe7da95
Janet Karpenske <janetkarpenske@gmail.com>
2020-09-16 12:03:55 -0700

    Fix functions to display player and monster stats

--- src/js/display_output.js
+++ src/js/display_output.js
@@ -3,9 +3,10 @@ import $ from 'jquery';
 // delete lines after a while 
 
 export default class Display {
-  
-  static output(output) {    
-    
+
+  static output(output) {
+
     $("#terminalOutput").append("<br>>" + output);
+  
   }
 }
