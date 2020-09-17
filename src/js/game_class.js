eee2a1b474b363cfb6c42373c9e5df5d4a5b892c
Ben Stoller <stollersystemsound@gmail.com>
2020-09-17 13:10:33 -0700

    add help file

--- src/js/game_class.js
+++ src/js/game_class.js
@@ -148,6 +148,11 @@ export default class Game {
         Display.output("Loot what?")  
       }
     }
+
+    //help
+    if (splitString[0] === "--help"||splitString[0] === "?"||splitString[0] === "help") {
+      this.help();
+    }
   }
   //look(target);
   look(target) {
@@ -383,4 +388,16 @@ export default class Game {
     }
     this.updateInvDisplay();
   }
+
+  help() {
+    Display.output(`<span class="white"><HELP FILE>
+    Hello adventurer! A little hooked are ya? No worries! Here's a list of commands that could be helpful!
+    <span class="cyan"><br>Commands:</span><br>
+    <movement> -> type "move"<br>
+    <sight-based> -> type "look" (looks at room.) or type "look at ___" to look at something specific.<br>
+    <combat> -> type "attack *enemy*" or "fight *enemy*" If an enemy is not specified, you will be asked what you're attacking.<br>
+    <looting> type "loot corpse" when an enemy is defeated. If an item is present in the environment, type "get item"<br>
+    <equip> type <equip item *body part*> to equip an item to the appropriate slot on your character.</span>`)
+  }
+  
 }
