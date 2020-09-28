80b4f6478ca8abab72068f0c3009064b8d871b5d
3Emme <emmettbuentiempo@gmail.com>
2020-09-27 21:51:08 -0700

    add examples for item instantiation

--- src/js/item_class.js
+++ src/js/item_class.js
@@ -19,6 +19,7 @@ export class Weapon extends Item {
     this.slot = slot;
   }
 }
+// let dagger = new Weapon("mainHand",["str",0], [1,"d",6],"Rusty Dagger", 2, 1, 5, 1, [], [], "common");
 
 export class Armor extends Item {
   constructor(slot,acBonus,type,name,Id,worth,Hp,level,status,flags,rarity) {
@@ -28,7 +29,8 @@ export class Armor extends Item {
     this.slot = slot;
   }
 }
- 
+// let rottingBoots = new Armor("legs", 1, "light", "Rotting Boots",1,1,5,1,[],[],"common");
+
 export class Container extends Item {
   constructor(type,capacity,name,Id,worth,Hp,level,status,flags,rarity) {
     super(name,Id,worth,Hp,level,status,flags,rarity);
@@ -37,6 +39,7 @@ export class Container extends Item {
     this.capacity = capacity;
   }
 }
+// let woodenChest1 = new Container("box",1000,"A wooden chest",1,500,30,1,[],[],"common");
 
 export class Consumable extends Item {
   constructor(action,type,name,Id,worth,Hp,level,status,flags,rarity) {
@@ -45,4 +48,4 @@ export class Consumable extends Item {
     this.type = type; 
   }
 }
-// ex: let healingPotion1 = new Consumable (["heal","self",1,"d",8,1],"potion","Healing Potion",1,100,1,5,[],["consume on use"],"common")
+// ex: let healingPotion1 = new Consumable (["heal","self",1,"d",8,1],"potion","Healing Potion",1,100,1,5,[],["consume on use"],"common");
